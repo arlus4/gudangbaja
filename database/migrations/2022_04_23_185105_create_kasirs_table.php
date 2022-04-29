7f2fc59914f1295c5b8a51055a208e3661d15272
arlus4 <arfan955@gmail.com>
2022-04-30 00:53:25 +0700

    Update

---
+++ database/migrations/2022_04_23_185105_create_kasirs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKasirsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kasirs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('kode')->unique();
+            $table->string('slug')->unique();
+            $table->string('nama');
+            $table->string('username')->unique();
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('is_kasir')->default(true);
+            $table->text('alamat');
+            $table->rememberToken();
+            $table->string('photo_ktp', 2048)->nullable();
+            $table->string('photo_profil', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kasirs');
+    }
+}
