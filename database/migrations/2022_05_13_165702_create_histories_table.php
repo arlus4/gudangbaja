919c769f8bd82c3dd5d9a80b771f85067a011888
arlus4 <arfan955@gmail.com>
2022-05-14 16:23:45 +0700

    Update

---
+++ database/migrations/2022_05_13_165702_create_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produk_id')->reference('id')->on('produk_hargas')->nullable();
+            $table->foreignId('agen_id');
+            $table->string('jumlah_produk', 10);
+            $table->string('ubah_produk', 10);
+            $table->string('tipe', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
