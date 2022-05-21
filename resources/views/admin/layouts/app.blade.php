eddf72d4969e8b9e92ad23ed9f500797a2dd6956
arlus4 <arfan955@gmail.com>
2022-05-21 16:16:00 +0700

    Update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
     <meta name="description" content="Web Gudangbaja" />
     <meta name="author" content="Gudangbaja" />
     <title>Gudang Baja | Prototype</title>
--- resources/views/home/layouts/main.blade.php
+++ resources/views/home/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
     <title>Gudangbaja | Prototype</title>
 
     <meta name="keywords" content="Marketplace ecommerce responsive HTML5 Template" />
